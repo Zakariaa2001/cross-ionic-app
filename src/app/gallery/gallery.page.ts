97afe9365699b90b2916571dd7110afa44b807a6
zakaria <hamzazakaria5555@gmail.com>
2024-01-06 19:11:30 +0100

    finsh gallery page

--- src/app/gallery/gallery.page.ts
+++ src/app/gallery/gallery.page.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gallery.page.scss'],
 })
 export class GalleryPage implements OnInit {
+  
+  keyword!:string;
+  currentPage:number=1;
+  size:number=10;
+  dataImages: any[] = [];
+  totalPages!:number;
+  
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
 
   ngOnInit() {
   }
 
+  onLoadImages(){
+    this.dataImages = [];
+    this.currentPage = 1;
+    this.totalPages =0;
+    this.doSearch();
+  }
+
+  doSearch() {
+    this.httpClient.get<ApiResponse>('https://pixabay.com/api/?key=41660342-81cc94bd7c50fe22dce799108&q=' + this.keyword + '&per_page=' + this.size + '&page=' + this.currentPage)
+    .subscribe(
+      (data) => {
+        data.hits.forEach(image => {
+          this.dataImages.push(image)
+          this.totalPages = data.totalHits/this.size;
+        })
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+
+  }
+
+  onIonInfinite(event:any) {
+    if (this.currentPage<this.totalPages) {
+      console.log(this.currentPage+"/"+this.totalPages)
+      ++this.currentPage;
+      this.doSearch();
+      event.target.complete()
+    }
+    if(this.currentPage>=this.totalPages) {
+      event.target.disabled = true;
+    }
+  }
+
+}
+interface ApiResponse {
+  totalHits: number;
+  hits: any[];
 }
